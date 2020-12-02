4554b8151d06692e86bd8f85dcfb77220d6d8477
Andrew McGarrity <amcgarrity@xcira.com>
2020-12-02 18:07:21 -0500

    reducer and actions with no api

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -4,19 +4,58 @@ const ADD_TODO = "todos/add";
 const COMPLETE_TODO = "todos/complete";
 const REMOVE_TODO = "todos/remove";
 
-function addTodo() {}
+function addTodo(content) {
+  return {
+    type: ADD_TODO,
+    payload: {
+      content,
+      completed: false,
+      id: Date.now(),
+      date: Date.now(),
+    },
+  };
+}
 
-function completeTodo() {}
+function completeTodo(id) {
+  return {
+    type: COMPLETE_TODO,
+    payload: id,
+  };
+}
 
-function removeTodo() {}
+function removeTodo(id) {
+  return {
+    type: REMOVE_TODO,
+    payload: id,
+  };
+}
 
 const initialState = [];
 
 function reducer(state = initialState, action) {
   switch (action.type) {
-    case ADD_TODO:
-    case COMPLETE_TODO:
-    case REMOVE_TODO:
+    case ADD_TODO: {
+      return [...state, action.payload];
+    }
+
+    case COMPLETE_TODO: {
+      const id = action.payload;
+
+      return state.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = true;
+        }
+
+        return todo;
+      });
+    }
+
+    case REMOVE_TODO: {
+      const id = action.payload;
+
+      return state.filter((todo) => todo.id !== id);
+    }
+
     default:
       return state;
   }
