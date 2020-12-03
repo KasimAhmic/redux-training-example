282d47c6830f44ed8733802d18c0c2380ff498fd
Andrew McGarrity <amcgarrity@xcira.com>
2020-12-03 10:44:30 -0500

    fix hot reload

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import todoReducer from "../features/todos/todosSlice";
 
 export default function configureAppStore() {
   const store = configureStore({
-    reducer: todoReducer,
+    reducer: { todos: todoReducer },
   });
 
   if (process.env.NODE_ENV !== "production" && module.hot) {
-    module.hot.accept("../features/todos/todosSlice", () => store.replaceReducer(todoReducer));
+    module.hot.accept("../features/todos/todosSlice", () =>
+      store.replaceReducer({ todos: todoReducer })
+    );
   }
 
   return store;
